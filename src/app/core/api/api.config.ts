733ff0071da5646b9b0fb95204b60c0e1eec431b
Clara Costa <claracostarc@gmail.com>
2026-05-05 10:09:17 -0300

    feat: implementa ajustes na hora e conexões para maior intuitividade

--- src/app/core/api/api.config.ts
+++ src/app/core/api/api.config.ts
@@ -2,5 +2,10 @@ import { InjectionToken } from '@angular/core';
 
 export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL', {
   providedIn: 'root',
-  factory: () => 'http://localhost/api',
+  factory: () => 'https://memory.techsoul.com.br/api',
 });
+
+// export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL', {
+//   providedIn: 'root',
+//   factory: () => 'http://localhost/api',
+// });
